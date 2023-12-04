a4439e1a4e586f767b143a48f913395e6b787477
diego <726167@student.dusd.net>
2023-12-04 13:52:45 -0800

    expanded the ocean by a lot LOL!!!
    - more water
    - damage tile can now be scaled
    - enemies soon
    - removed herobrine

--- RubysAdventureGarcia/Assets/2DBeginnerTutorialResources/Scripts/RubyController.cs
+++ RubysAdventureGarcia/Assets/2DBeginnerTutorialResources/Scripts/RubyController.cs
@@ -32,6 +32,13 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+        
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0 )
+                isInvincible = false;
+        }
     }
     void FixedUpdate()
     {
@@ -43,7 +50,15 @@ public class RubyController : MonoBehaviour
     }
     public void ChangeHealth(int amount)
     {
-        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
+            currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
 }

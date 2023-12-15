db46b51c43fba9451a84cf3569da028a741c931b
diego <726167@student.dusd.net>
2023-12-14 17:13:25 -0800

    Smoke. dancing, and more!!!@1!!1!11
    - added follow cam
    - added dance animation for robot
    - added smoke effect
    - removed herobrine

--- RubysAdventureGarcia/Assets/2DBeginnerTutorialResources/Scripts/EnemyController.cs
+++ RubysAdventureGarcia/Assets/2DBeginnerTutorialResources/Scripts/EnemyController.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,7 @@ public class EnemyController : MonoBehaviour
     public float speed;
     public bool vertical;
     public float changeTime = 3.0f;
-
+    public bool broken = false;
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
@@ -22,6 +23,11 @@ public class EnemyController : MonoBehaviour
 
     private void Update()
     {
+        if (!broken)
+        {
+            return;
+        }
+
         timer -= Time.deltaTime;
         
         if (timer < 0)
@@ -36,6 +42,11 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+       if (!broken)
+        {
+            return;
+        }
+        
         Vector2 position = rigidbody2D.position;
 
         if (vertical)
@@ -52,13 +63,20 @@ public class EnemyController : MonoBehaviour
         }
         rigidbody2D.MovePosition(position);
     }
-    private void OnCollisionEnter2D(Collision2D other)
+    void OnCollisionEnter2D(Collision2D other)
     {
         RubyController player = other.gameObject.GetComponent<RubyController>();
         if (player != null)
         {
             player.ChangeHealth(-1);
         }
+       
+    }
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+        animator.SetTrigger("Fixed");
     }
 
 }
--- RubysAdventureGarcia/Assets/2DBeginnerTutorialResources/Scripts/Projectile.cs
+++ RubysAdventureGarcia/Assets/2DBeginnerTutorialResources/Scripts/Projectile.cs
@@ -15,17 +15,25 @@ public class Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
     }
     public void Launch(Vector2 direction, float force)
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
         Debug.Log("Projectile direction + force " + direction + " / " + force);
         rigidbody2d.AddForce(direction * force);
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("Projectile Collision with " + other.gameObject);
+        EnemyController e = other.collider.GetComponent<EnemyController>();
+        if (e != null)
+        {
+            e.Fix();
+        }
         Destroy(gameObject);
     }
 
